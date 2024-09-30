7af8211b2217682f3c5cb2c56a65e1b094cd7843
janees07 <janeesleo55@gmail.com>
2024-09-30 11:08:11 +0530

    Restored deleted files and added untracked files

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const {getBookIssuedToUser,getAllUsers} = require("../controller/userController")
-
-router.get("/",getAllUsers)
-router.get("/books",getBookIssuedToUser)
-
-module.exports = router;
